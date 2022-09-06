4c1123bc11ed87de4fe0e918e6236f52e686d6a0
Carter McCall <Carter_McCall53@outlook.com>
2022-09-06 12:51:50 -0600

    Add project files.

---
+++ TemperatureConverter/TemperatureConverter.cpp
@@ -0,0 +1,34 @@
+/* TemperatureConverter.cpp
+ * Carter McCall
+ * 6 September 2022
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    char repChar;
+    string repeat;
+    do
+    {
+        double convertedTemp, originalTemp;
+        char conversion, charConversion;
+        cout << "Would you like to convert F to C or C to F? ";
+        cin >> conversion;
+        charConversion = tolower(conversion);
+        cout << "\nWhat temperature would you like to convert? ";
+        cin >> originalTemp;
+            if (charConversion == 'f')
+            {
+                convertedTemp = (originalTemp - 32) * 5 / 9;
+            }
+            else if (charConversion == 'c')
+            {
+                convertedTemp = (originalTemp * 9 / 5) + 32;
+            }
+        cout << "Your new temperature is " << convertedTemp << "!" << endl;
+        cout << "\n" << endl;
+        cout << "Would you like to make another conversion? (Y|N): ";
+        cin >> repeat;
+        repChar = tolower(repeat[0]);
+    } while (repChar == 'y');        
+}
